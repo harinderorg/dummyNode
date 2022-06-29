5888ffc4078258a006165ef64746dd68decf616d
Your Name <harinder.cipio.ai>
2022-06-29 19:44:05 +0530

    latest

--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -1,5 +1,5 @@
 "use strict";
 module.exports = function (app) {
   var users = require("../controllers/UsersCtrl");
-  app.route("/signup").get(users.signup);
+  app.route("/signup").post(users.signup);
 };
